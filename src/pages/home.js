292cd06cc7af17b300182d8c3455636c8e722ea5
Pedro Sobral <pedrolusobral85@gmail.com>
2026-05-12 22:34:14 -0300

    feat: add feature section

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,27 +5,51 @@ export default function loadHome(){
     const content = document.querySelector("#content");
 
     content.appendChild(createHeroSection());
+    content.appendChild(createFeatureSection());
 }
 
 function createHeroSection(){
     const heroSection = createElement("section", "hero-section", "");
 
-    heroSection.appendChild(createTitleHero());
-    heroSection.appendChild(createSubtitleHero());
+    heroSection.appendChild(createHeroTitle());
+    heroSection.appendChild(createHeroSubtitle());
     heroSection.appendChild(createElement("img", "hero-image", "", {src: heroImage}));
 
     return heroSection;
 }
 
-function createTitleHero(){
+function createHeroTitle(){
     return createElement("h2", "hero-title", "Taste the Best Food in Town");
 }
 
-function createSubtitleHero(){
+function createHeroSubtitle(){
     const subtitle = createElement("div", "hero-subtitle", "");
     
     subtitle.appendChild(createElement("p", "p-subtitle", "Fresh ingredients. Handmade dishes. Memorable experiences."));
     subtitle.appendChild(createElement("button", "button-subtitle menu", "View Menu"));
     
     return subtitle;
+}
+
+function createFeatureSection(){
+    const featureSection = createElement("section", "feature-section", "");
+
+    featureSection.appendChild(createFeatureTitle());
+    featureSection.appendChild(createCards());
+
+    return featureSection;
+}
+
+function createFeatureTitle(){
+    return createElement("h2", "feature-title", "Why Choose Us?", "");
+}
+
+function createCards(){
+    const container = createElement("div", "feature-container-cards", "", "");
+
+    container.appendChild(createElement("p", "feature-card", "We use high-quality fresh ingredients every day to deliver authentic flavors.", ""));
+    container.appendChild(createElement("p", "feature-card", "A warm and welcoming space perfect for friends, family, and special moments.", ""));
+    container.appendChild(createElement("p", "feature-card", "Our chefs combine passion and creativity to craft unforgettable dishes.", ""));
+
+    return container;
 }
